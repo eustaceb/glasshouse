85aa0e11ca542f33a740185847efaf7698f99baf
Justas <Justas.B94@gmail.com>
2020-10-31 14:28:13 +0000

    Add React and Material-UI
    Switched to React + Material-UI for an improved UX and modularisation.
    Reimplemented the previous functionality with React, bar for the
    distortion amount input.
    Updated readme and package.json.

--- src/index.js
+++ src/index.js
@@ -1,42 +1,82 @@
 import * as Tone from "tone";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from '@material-ui/core';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
 
-var players = [];
 
-const samples = {
-    "hihats": "audio/hihats.mp3",
-    "synth": "audio/synth.mp3"
-  };
-
-document.getElementById("sample-play").addEventListener("click", function(){
-    const selectedSample = document.getElementById("sample-select").value;
-    const distortionEnabled = document.getElementById("distortion").checked;
-    const loopSample = document.getElementById("loop").checked;
-    const distortionAmount = document.getElementById("distortion-amount").value;
-
-    var player = new Tone.Player(samples[selectedSample]).toDestination();
-    players.push(player);
-
-    if (distortionEnabled)
+const samples = [
     {
-        const distortion = new Tone.Distortion(distortionAmount).toDestination();
-        player.connect(distortion);
+        "name": "hihats",
+        "path": "audio/hihats.mp3",
+        "buffer": new Tone.Buffer("audio/hihats.mp3"),
+    },
+    {
+        "name": "synth",
+        "path": "audio/synth.mp3",
+        "buffer": new Tone.Buffer("audio/synth.mp3"),
+    },
+];
+
+function ListItemLink(props) {
+    return <ListItem button component="a" {...props} />;
+}
+
+function SampleList(props) {    
+    const samples = props.samples.map((sample) =>
+        <ListItemLink key={sample.name} onClick={() => props.selectSample(sample.name)}>
+            <ListItemText primary={sample.name}/>
+        </ListItemLink>
+    );
+
+    return (
+        <List component="nav">
+            {samples}
+        </List>
+    );
+}
+
+function SamplePlayer(props) {
+    const [selectedSample, setSample] = useState('');
+    const [distortionEnabled, enableDistortion] = useState(false);
+    const [distortionAmount, setDistortionAmount] = useState(0.0);
+    const [loopSample, enableLoop] = useState(false);
+
+    const playSample = (sampleName) => {
+
+        const sampleIndex = samples.findIndex(obj => obj.name == sampleName);
+
+        console.log("Playing sample " + samples[sampleIndex].name);
+
+        var player = new Tone.Player(samples[sampleIndex].buffer).toDestination();
+    
+        if (distortionEnabled) {
+            const distortion = new Tone.Distortion(distortionAmount).toDestination();
+            player.connect(distortion);
+        }
+    
+        if (loopSample) {
+            player.loop = true;
+        }
+        console.log("sampleName: %s, sampleIndex: %d, distortionEnabled: %s, distortionAmount: %d, loopSample: %s", sampleName, sampleIndex, distortionEnabled, distortionAmount, loopSample);
+        Tone.loaded().then(() => {
+            player.start();
+        });
     }
 
-    if (loopSample)
-    {
-        player.loop = true;
-    }
+    return (
+        <div>
+            <SampleList samples={props.samples} selectSample={(name) => {console.log("Selecting" + name); setSample(name)}} />
+            <Checkbox label="Enable Distortion" onChange={e => enableDistortion(e.target.checked)} />
+            <TextField label="Distortion Amount" type="number" />
+            <Checkbox value="loop" onChange={e => enableLoop(e.target.checked)} />
+            <Button variant="contained" color="primary" onClick={() => playSample(selectedSample)}>Play Sample</Button>
+        </div>
+    )
+}
 
-    Tone.loaded().then(() => {
-        player.start();
-    });
-
-});
-
-document.getElementById("reset").addEventListener("click", function(){
-    players.forEach(function(player){
-        player.dispose();
-    });
-
-    players = [];
-});
+ReactDOM.render(<SamplePlayer samples={samples} />, document.getElementById('samplePlayer'));
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,20 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: ['.js', '.jsx'],
+        },
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
