4d27b8bbe32cbc23ada91fa877cbc43dcc4935be
Justas <Justas.B94@gmail.com>
2021-05-08 15:17:22 +0100

    Painting functionality for Sequencer
    The sequencer can now be painted with selected samples.

--- src/components/Sequencer.js
+++ src/components/Sequencer.js
@@ -2,34 +2,42 @@ import * as Tone from "tone";
 import React, {useState} from "react";
 
 export function SequencerCell(props) {
-  const [sampleId, setSampleId] = useState(null);
-  const [active, setActive] = useState(false);
   const styles = {
     cell: {
-      backgroundColor: "grey",
       width: 50,
       height: 50,
       borderRadius: 2,
-    },
-    active: {
-      backgroundColor: "black",
     },
   };
 
   return (
     <div
-      onClick={() => setActive(!active)}
-      style={active ? {...styles.cell, ...styles.active} : styles.cell}></div>
+      onClick={() => props.paint()}
+      style={{...styles.cell, "backgroundColor": props.color}}></div>
   );
 }
 
 export function Sequencer(props) {
   const cols = 16;
   const rows = 2;
-  const [grid, setGrid] = useState(Array(rows).fill(Array(cols).fill(0)));
+  const [grid, setGrid] = useState(Array(rows).fill(Array(cols).fill(-1)));
+
+  function paintCell(row, col, sampleIndex) {
+    setGrid(
+      grid.map((gridRow, i) => {
+        return gridRow.map((gridCell, j) => {
+          if (i === row && j === col) {
+            if (gridCell === sampleIndex) return -1;
+            else return sampleIndex;
+          }
+          return gridCell;
+        });
+      })
+    );
+  }
 
   return (
-    <table style={{"margin": "0 auto"}}>
+    <table style={{margin: "0 auto"}}>
       <tbody>
         {grid.map((row, i) => {
           return (
@@ -37,7 +45,10 @@ export function Sequencer(props) {
               {row.map((cell, j) => {
                 return (
                   <td key={"row" + i.toString() + "col" + j.toString()}>
-                    <SequencerCell />
+                    <SequencerCell
+                      paint={() => paintCell(i, j, props.selectedSampleIndex)}
+                      color={ cell === -1 ? "grey" : props.samples[cell]["color"]}
+                    />
                   </td>
                 );
               })}
