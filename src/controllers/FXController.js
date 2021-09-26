b07b03e58bde49f2f8f2d8a7831e4077a6d2cbd9
Justas <Justas.B94@gmail.com>
2021-09-26 21:34:17 +0100

    Add FxParam class for managing effect parameters
    This should help us deal with the ever-growing number of effect param
    properties.

--- src/controllers/FXController.js
+++ src/controllers/FXController.js
@@ -1,14 +1,27 @@
 import * as Tone from "tone";
 
-// class FxParam {
-//   constructor(name, displayName, min, max, initialValue) {
-//     this.name = name;
-//     this.displayName = displayName;
-//     this.min = min;
-//     this.max = max;
-//     this.value = initialValue;
-//   }
-// }
+class FxParam {
+  /**
+   * Class that represents an FX parameter
+   * @param {*} name Name of the effect
+   * @param {*} displayName Name to be displayed on the UI
+   * @param {*} type At the moment there are three types: "range", "toggle"
+   * @param {*} initialValue Starting value
+   * @param {*} values A range [min, max] if type is "range", set of permitted values otherwise
+   */
+  constructor(name, displayName, type, values, initialValue, readonly = false) {
+    this.name = name;
+    this.displayName = displayName;
+    this.type = type;
+    if (type === "range") {
+      [this.min, this.max] = values;
+    } else {
+      this.values = values;
+    }
+    this.value = initialValue;
+    this.readonly = readonly;
+  }
+}
 
 class Fx {
   constructor(name, params, fnEnabledCheck) {
@@ -36,29 +49,38 @@ export class FXController {
       chorus: this.createChorusFx,
     };
 
+    // Some commonly used parameters
+    const createAmountParam = () =>
+      new FxParam("amount", "Amount", "range", [0, 100], 0);
+    const createEnabledParam = () =>
+      new FxParam("enabled", "Enabled", "toggle", [true, false], false);
+
     this.effects = {};
     this.effects["distortion"] = new Fx(
       "distortion",
-      {amount: {range: [0, 100], value: 0}, enabled: {value: false}},
-      (params) => params["enabled"]["value"] === true && params["amount"]["value"] > 0
+      {amount: createAmountParam(), enabled: createEnabledParam()},
+      (params) =>
+        params["enabled"]["value"] === true && params["amount"]["value"] > 0
     );
     this.effects["reverb"] = new Fx(
       "reverb",
-      {amount: {range: [0, 100], value: 0}, enabled: {value: false}},
-      (params) => params["enabled"]["value"] === true && params["amount"]["value"] > 0
+      {amount: createAmountParam(), enabled: createEnabledParam()},
+      (params) =>
+        params["enabled"]["value"] === true && params["amount"]["value"] > 0
     );
     this.effects["delay"] = new Fx(
       "delay",
-      {amount: {range: [0, 100], value: 0}, enabled: {value: false}},
-      (params) => params["enabled"]["value"] === true && params["amount"]["value"] > 0
+      {amount: createAmountParam(), enabled: createEnabledParam()},
+      (params) =>
+        params["enabled"]["value"] === true && params["amount"]["value"] > 0
     );
     this.effects["chorus"] = new Fx(
       "chorus",
       {
-        frequency: {range: [0, 10], value: 0},
-        delayTime: {range: [0, 100], value: 0},
-        depth: {range: [0, 1], value: 0},
-        enabled: {value: false},
+        frequency: new FxParam("frequency", "Frequency", "range", [0, 10], 0, true),
+        delayTime: new FxParam("delayTime", "Delay Time", "range", [0, 100], 0),
+        depth: new FxParam("depth", "Depth", "range", [0, 1], 0),
+        enabled: createEnabledParam(),
       },
       (params) => params["enabled"]["value"]
     );
@@ -71,14 +93,27 @@ export class FXController {
     fx.params[parameter]["value"] = value;
     const enabled = fx.isEnabled();
 
+    // Need to reinstate the node if a param is readonly
+    let reconnect = false;
+    if (fx.params[parameter].readonly) {
+      reconnect = true;
+    }
+
     // If we're disabling an effect, simply disconnect it and update state
     if (fx.isEnabled()) {
-      if (prevEnabled !== enabled) {
+      // Modified a readonly property - need to reocnnect
+      if (reconnect) {
+        console.log(`Reconnecting effect ${fxName}`);
+        this.player.disconnect(fx.node);
+      }
+
+      // Effect was disabled previously but is being enabled
+      if (prevEnabled !== enabled || reconnect) {
         console.log(`Connecting effect ${fxName}`);
         fx.node = this.createFxCallbacks[fxName](fx.params);
         this.player.connect(fx.node);
-      }
-      if (parameter !== "enabled") {
+      } else {// if (parameter !== "enabled" && !reconnect) {
+        // Only need to write value to node if we've not reconnected
         fx.node[parameter] = value;
       }
     } else if (prevEnabled !== enabled && fx.node !== null) {
