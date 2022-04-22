195daece248b7d1137d176ab0a1058a66d2c6d4f
Justas <Justas.B94@gmail.com>
2022-04-22 22:35:11 +0100

    Make samples from groups continue in new sections
    This is just an attempt to make it such that when you change sections
    a sample from a group would be swapped with the corresponding sample
    from the next section. Quite buggy at the moment.

--- src/controllers/Composition.js
+++ src/controllers/Composition.js
@@ -80,6 +80,11 @@ export class Composition {
           return sampleController.getSampleByName(sampleName);
         });
 
+        // Indicate which group this sample belongs to for transitions
+        samples.forEach((s, i) => {
+          s.setGroupInfo([name, i]);
+        });
+
         const preFxData = "preFx" in groupData ? groupData["preFx"] : null;
         const fxData = groupData["fx"];
 
@@ -105,19 +110,20 @@ export class Composition {
                   fxData["switch"]["options"]
                 )
               : null;
-          const generateRange = (from, to) =>
-            Array.from({length: to - from + 1}, (_, k) => from + k);
-          const sliderControl =
-            "slider" in fxData
-              ? new DiscreteControl(
-                  fx,
-                  fxData["slider"]["paramName"],
-                  generateRange(
-                    fxData["slider"]["range"][0],
-                    fxData["slider"]["range"][1]
-                  )
-                )
-              : null;
+
+          const createSlider = (data) => {
+            if (!("slider" in data)) return null;
+            const generateRange = (from, to) =>
+              Array.from({length: to - from + 1}, (_, k) => from + k);
+            const sliderData = data["slider"];
+            const options =
+              "options" in sliderData
+                ? sliderData["options"]
+                : generateRange(sliderData["range"][0], sliderData["range"][1]);
+            return new DiscreteControl(fx, sliderData["paramName"], options);
+          };
+          const sliderControl = createSlider(fxData);
+
           return {
             wet: wetControl,
             xy: xyControl,
