9b72b922548ab48af1ca39b36287280c81fa0c58
Justas <Justas.B94@gmail.com>
2022-04-24 19:00:36 +0100

    Add additional styling for Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,27 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
+import * as Tone from "tone";
 
 export function Navigation(props) {
   const sectionIndex = props.sectionIndex;
+  const [muted, setMuted] = useState(false);
+
+  const mute = () => {
+    Tone.getDestination().mute = !Tone.getDestination().mute;
+    setMuted(!muted);
+  };
+
   return (
     <div className="navigation">
       {sectionIndex > 0 ? (
         <div
-          className="navigationArrowLeft"
+          className="navigationArrow navigationArrowLeft"
           onClick={() => props.setSection(sectionIndex - 1)}
         />
       ) : (
-        <div className="navigationMarkerLeft" />
+        <div className="navigationMarker navigationMarkerLeft" />
       )}
       <div className="navigationRepeat" />
-      <div className="navigationMute"/>
+      <div
+        className={"navigationMute" + (muted ? " navigationMuteActive" : "")}
+        onClick={mute}
+      />
       <div className="navigationRepeat" />
       {sectionIndex < props.sectionCount - 1 ? (
         <div
-          className="navigationArrowRight"
+          className="navigationArrow navigationArrowRight"
           onClick={() => props.setSection(sectionIndex + 1)}
         />
       ) : (
-        <div className="navigationMarkerRight" />
+        <div className="navigationMarker navigationMarkerRight" />
       )}
     </div>
   );
