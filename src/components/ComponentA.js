5726744c053f368cc7ad8baf487c937c34d3839f
Danielius Sukys <sukys.danielius@gmail.com>
2022-04-24 14:17:59 +0100

    Positioned vocal 2

--- src/components/ComponentA.js
+++ src/components/ComponentA.js
@@ -3,8 +3,8 @@ import React, {useState, useEffect} from "react";
 export function ComponentA(props) {
   return (
     <div className={"component componentA"}>
-      <div className={"vocal vocal1_s1"}>
-      </div>
+      <div className={"vocal vocal1_s1"}></div>
+      <div className={"vocal vocal2_s1"}></div>
     </div>
   )
 }
