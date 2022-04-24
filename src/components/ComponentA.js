0bbdeb6ef54bf0fbcb0a143b2843eb8508f29dad
Danielius Sukys <sukys.danielius@gmail.com>
2022-04-24 16:16:02 +0100

    implemented downslider

--- src/components/ComponentA.js
+++ src/components/ComponentA.js
@@ -1,10 +1,21 @@
 import React, {useState, useEffect} from "react";
-
+import {DownSlider} from "./DownSlider.js"
 export function ComponentA(props) {
+
+
   return (
     <div className={"component componentA"}>
       <div className={"vocal vocal1_s1"}></div>
       <div className={"vocal vocal2_s1"}></div>
+      <div className={"vocal vocal3_s1"}></div>
+      <DownSlider
+        minStep="0"
+        maxStep="7"
+        minPosition="0"
+        maxPosition="100"
+        initialPosition="40"
+        initialStep="2"
+        className="downSlider1" ></DownSlider>
     </div>
   )
 }
---
+++ src/components/DownSlider.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from "react";
+import {Slider} from "@material-ui/core";
+
+export function DownSlider(props) {
+
+  // position describes continuous output to control the effect
+  // step describes discrete state of the slider
+
+  const minStep = parseInt(props.minStep)
+  const maxStep = parseInt(props.maxStep)
+  const minPosition = parseInt(props.minPosition)
+  const maxPosition = parseInt(props.maxPosition)
+  const initialPosition = parseInt(props.initialPosition)
+  const initialStep = parseInt(props.initialStep)
+  const className = props.className
+
+  const [step, setStep] = useState(initialStep);
+  const [position, setPosition] = useState(initialPosition);
+  const [mouseDown, setMouseDown] = useState(false);
+
+  const handleMouseDown = (event) => {
+    setMouseDown(true);
+    // console.log(1);
+  };
+
+  const handleMouseUp = (event) => {
+    setMouseDown(false);
+    // console.log(0);
+  };
+
+  const handleMouseMove = (event) => {
+
+    if(mouseDown) {
+      let nextPosition = position + event.movementY;
+      nextPosition = Math.max(Math.min(maxPosition, nextPosition), minPosition);
+
+      setPosition(nextPosition);
+      setStep(Math.trunc(position / maxPosition * maxStep));
+      console.log(position)
+    }
+  };
+
+
+  return (
+    <div 
+      className={className + " " + className + "_" + step}
+      onMouseDown={ handleMouseDown } 
+      onMouseUp={ handleMouseUp }
+      onMouseMove={handleMouseMove}
+    ></div>
+  );
+}
